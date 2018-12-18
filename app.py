a9e7e0a9316cdec6b7a60ff3ff8589a65185e6e2
handq <haqsir@163.com>
2018-12-18 22:40:44 +0800

    处理空格

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
+
+
+@app.route('/', methods = ['GET', 'POST'])
 def home():
     return '<h1>home</h1>'
+
 
 @app.route('/signin', methods=['GET'])
 def signin_from():
@@ -16,9 +19,10 @@ def signin_from():
               <p><button type="submit">sign in</button></p>
                </form>'''
 
+
 @app.route('/signin', methods=['POST'])
 def signin():
-    if request.form['username']=='admin' and request.form['password']=='admin':
+    if request.form['username'] == 'admin' and request.form['password'] == 'admin':
         print (request)
         return '<h3>hello admin</h3>'
     return '<h3>bad username or password</h3>'
